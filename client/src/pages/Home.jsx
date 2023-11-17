17c7bc78f25aee85765f55915c0b2301c43ee7e8
asoto225 <abelsoto12@hotmail.com>
2023-11-16 22:18:05 -0700

    created inital router to display welcome page

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+      <div id="home">
+        <h1>Welcome to the app!</h1>
+        <p>
+          Hello World! 
+        </p>
+      </div>
+    );
+}
