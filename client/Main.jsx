3df1eb5b3a11e78000a6912886aca07c894604eb
Domitius <133902698+recenasu@users.noreply.github.com>
2023-11-24 10:36:21 -0700

    CreateAccount link debug

--- client/Main.jsx
+++ client/Main.jsx
@@ -5,6 +5,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx'
 import Home from './src/pages/Home.jsx'
 import Error from './src/pages/Error.jsx'
+import CreateAccount from './src/pages/CreateAccount.jsx';
 
 const router = createBrowserRouter([
   {
@@ -16,6 +17,11 @@ const router = createBrowserRouter([
         // need to change to login page later once set 
         index: true,
         element: <Home />,
+      },
+      {
+        // need to change to login page later once set 
+        path: '/create-account',
+        element: <CreateAccount />,
       },
     //   {
     //     path: 'home',
