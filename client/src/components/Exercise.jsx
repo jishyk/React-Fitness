143c0f19f79f0f3e58c2e18a34a7efa49a95012d
Joshua King <joshua-coding@Joshuas-MacBook-Pro.local>
2023-11-25 21:59:49 -0700

    some CSS for Nutrition, Goal, and workout pages

---
+++ client/src/components/Exercise.jsx
@@ -0,0 +1,9 @@
+<div className="add-workout-container">
+            <div className="add-workout-box">
+                <h2>Add Workout</h2>
+                {/* Form or content for adding a workout */}
+                <Link to="/dashboard" className="back-to-dashboard-button">Back to Dashboard</Link>
+            </div>
+        </div>
+
+export default AddWork;
